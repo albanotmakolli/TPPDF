bd391d3ad8ca36852775dfd3b661388b73867ad6
Albanot Makolli <albanotmakolli@gmail.com>
2020-05-30 21:18:16 +0200

    extend sequence with KeyPath map

--- Source/Internal/JSON/Array+PDFJSONSerializable.swift
+++ Source/Internal/JSON/Array+PDFJSONSerializable.swift
@@ -26,3 +26,9 @@ extension Array: PDFJSONSerializable {
     }
 
 }
+
+extension Sequence {
+    func map<T>(_ keyPath: KeyPath<Element, T>) -> [T] {
+        return map { $0[keyPath: keyPath] }
+    }
+}
